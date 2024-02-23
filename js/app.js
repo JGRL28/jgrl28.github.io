f694a4f5e142b885e9a5bcd7ce49a01f70996f2e
Jose Rodriguez <jose28206892@gmail.com>
2024-02-23 00:22:55 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -4,3 +4,20 @@ const navMenu = document.querySelector(".nav-menu")
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible")
 })
+
+const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
+
+menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener("click", function () {
+        navMenu.classList.remove("nav-menu_visible")
+    })
+})
+
+//  Selector con id 
+
+document.getElementById("navegadorSelect").addEventListener("change", function () {
+    var selectedOption = this.value;
+    if (selectedOption !== "-- Elige una sección --") {
+        window.location.href = selectedOption;
+    }
+});
