15dedd5c32ad0fb8eeb5ed9d9144bf1f7ef5b4f3
Jose Rodriguez <jose28206892@gmail.com>
2024-06-10 02:07:49 -0400

    commit

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible")
-})
-
-const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
-
-menuLinks.forEach(menuLinks => {
-    menuLinks.addEventListener("click", function () {
-        navMenu.classList.remove("nav-menu_visible")
-    })
-})
-
-//  Selector con id 
-
-document.getElementById("navegadorSelect").addEventListener("change", function () {
-    var selectedOption = this.value;
-    if (selectedOption !== "-- Elige una sección --") {
-        window.location.href = selectedOption;
-    }
-});
-
 // Slider
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -68,3 +44,5 @@ document.addEventListener("DOMContentLoaded", function () {
         showSlide(currentIndex);
     }, 10000);
 });
+
+
